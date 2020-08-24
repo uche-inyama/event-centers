830688f4591dcbf51fec84b5d7e0443a40d84022
uche-inyama <depoulet@yahoo.com>
2020-08-25 00:34:19 +0100

    add provider & store to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './rootReducer.js';
+import { Provider } from 'react-redux';
+import { requestCenters } from './actions';
 import './index.css';
 import App from './App';
 
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
+
+store.subscribe(() => {
+  console.log('state modified - through reducers');
+  console.log(store.getState());
+});
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
--- src/reducer/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import eventCenters from './eventCenterReducer';
-
-export default combineReducers({
-  eventCenters
-})
