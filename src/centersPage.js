4530f1cd7bb4c74ead8fd9024321d507925e1df2
uche-inyama <depoulet@yahoo.com>
2020-08-25 00:53:34 +0100

    implement component wrap list of centers

---
+++ src/centersPage.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
+import CenterList from './centerList';
+import { requestCenters, deleteCenter } from './actions';
+
+
+const CentersPage = ({ centers, fetchCenters, deleteCenter }) => {
+  console.log('centers page loaded');
+  console.log(centers)
+  // useEffect(() => {
+  //   console.log('use effect - fetch centers');
+  //   fetchCenters();
+  // }, [])
+
+  return (
+    <div>
+      <h2>Centers Page</h2>
+      <Link to="/center/new">Add Center</Link>
+      <CenterList
+        centers={centers}
+        deleteCenter={deleteCenter}
+      />
+    </div>
+  )
+}
+
+
+const mapStateToProps = (state) => {
+  return {
+    centers: state.centers
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchCenters: (() => {
+      dispatch(requestCenters());
+    }),
+    deleteCenter: ((id) => {
+      dispatch(deleteCenter(id))
+    })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CentersPage);
