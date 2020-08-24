ad051a46fc25a2f46e20a7522ef8fa519f0eafb8
uche-inyama <depoulet@yahoo.com>
2020-08-25 00:47:24 +0100

    re-implement reducers

---
+++ src/reducers/centers.js
@@ -0,0 +1,21 @@
+import { SET_CENTERS, POST_CENTERS, CENTER_UPDATED, CENTER_DELETED } from '../actions'
+
+const centers = (state = [], action) => {
+  switch (action.type) {
+    case SET_CENTERS:
+      return [...state, ...action.centers];
+    case POST_CENTERS:
+      return [...state, action.centers];
+    case CENTER_UPDATED:
+      return state.map(item => {
+        if (item.id === action.center.id) return action.center;
+        return item;
+      });
+    case CENTER_DELETED: {
+      return state.filter(x => x.id !== action.centerId);
+    }
+    default:
+      return state;
+  }
+}
+export default centers;
