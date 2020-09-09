f1e8f081f113333810405ba21c3e7d0b25f56b9d
uche-inyama <depoulet@yahoo.com>
2020-09-09 11:08:51 +0100

    add style to create center form

--- src/admin/components/centerPage/centersPageStyle.js
+++ src/admin/components/centerPage/centersPageStyle.js
@@ -1,11 +1,23 @@
 import styled from 'styled-components';
 
 const CentersPageWrapper = styled.div`
+position: relative;
 font-family: 'Montserrat', sans-serif;
-  h2, .add-center{
-    padding: 20px;
+  h2{
+    padding: 20px 20px 20px 30px;
   }
- 
+  .add-center {
+    border: 1px solid blue;
+    height: 50%;
+    text-decoration: none;
+    position: relative;
+    padding: 5px;
+    left: 35px;
+    top: 10px;
+  
+  }
+  width: 768px;
+  margin: 0 auto;
 `;
 
 export default CentersPageWrapper;
--- src/admin/components/newCenterForm/newCenterFormStyle.js
+++ src/admin/components/newCenterForm/newCenterFormStyle.js
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 
 const NewCenterForm = styled.div`
+width: 576px;
+margin: 0 auto;
 font-family: 'Montserrat', sans-serif;
+h2{
+  padding-top: 20px;
+  text-align: center;
+}
 .field{
   display: grid;
   grid-template-rows: 1fr;
-  width: 40%;
+  input{
+    height: 35px;
+  }
   label{
     padding-top: 20px;
   }
